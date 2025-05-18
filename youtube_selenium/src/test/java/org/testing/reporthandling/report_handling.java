424cc3fb9e04a24d9780b5c160b74f7d480bb8e2
Nishant Kumar <Nishant Kumar@kajal-verma>
2025-05-18 10:14:14 +0530

    report

--- youtube_selenium/src/test/java/org/testing/reporthandling/report_handling.java
+++ youtube_selenium/src/test/java/org/testing/reporthandling/report_handling.java
@@ -8,7 +8,7 @@ import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
 public class report_handling {
 	public static ExtentSparkReporter report() {
-	ExtentSparkReporter report=new ExtentSparkReporter("C:\\Users\\Nishant Kumar\\Documents\\report.html");
+	ExtentSparkReporter report=new ExtentSparkReporter("/youtube_selenium/report.html");
 //	String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
 //	ExtentSparkReporter spark = new ExtentSparkReporter("reports/report_" + timestamp + ".html");
 //	ExtentSparkReporter report=new ExtentSparkReporter("C:\\\\Users\\\\Nishant Kumar\\\\Documents\\\\report"+timestamp+".html");
